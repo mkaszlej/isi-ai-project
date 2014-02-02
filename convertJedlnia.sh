7edb20bb8f764dfd12fce2328ca589450a250c29
Michał Kaszlej <mkaszlej@gmail.com>
2014-02-02 12:53:32 +0100

    Initial commit

---
+++ convertJedlnia.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#IFS=$'\n';
+cat $1 | perl -p -e 's/\t*//' | perl -p -e 's/\n(\n)/\1/' | perl -p -e 's/[\t ]*U-R-L (.*)\n/'$1'|||P|=HIPERŁĄCZE("\1"/' | perl -p -e 's/N-A-M-E (.*)\n/|"\1")|/' | perl -p -e 's/A-D-R-R-E-S-S (.*)\n/\1|/' | perl -p -e's/(.*)[\n]/\1|\n/' | perl -p -e 's/T-E-L//' | grep  '.\{5,\}'
+
+
+
---
+++ old/sh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+wget "http://www.zumi.pl/lista-firm/$1-$2" -q 0 -O t1.html
+grep "<li  itemprop=\"itemListElement\"><a href=\"" t1.html | sed 's/[ \t]*<li  itemprop=\"itemListElement\"><a href=\"/http:\/\/zumi.pl/' | sed 's/">.*//' > adresy.html
+#mamy adresy stron z ulicami
+for line in $(cat adresy.html) 
+do
+ time=$(grep -m1 -ao '[0-5]' /dev/urandom | sed s/0/10/ | head -n1)
+
+ echo "Pobieram adres: $line..." 1>&2
+
+ wget $line -q 0 -O - | grep "<a itemprop=\"\|<span itemprop=\"" | sed 's/[ \t]*<a itemprop="name" href="/U-R-L http:\/\/www.zumi.pl/' | sed 's/#!s=homePage">/\nN-A-M-E /' | sed 's/<a itemprop=\"url".*//' | sed 's/[ \t]*<a itemprop.*">/A-D-R-R-E-S-S /' | sed 's/<\/a><br \/>//' | sed 's/<\/a>//'| sed 's/[ \t]*<span itemprop="telephone">/T-E-L /' | sed 's/&nbsp;/ /' | sed 's/<\/span>/\n/' >> $3.raw
+
+ echo "$time - sekund oczekiwania..." 1>&2
+
+ sleep $time
+
+done
+dos2unix $3.raw
+sh convert.sh $3.raw $2 > $3
